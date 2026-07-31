b02ac79cbbb1a37b6cb29b3745e8829b0b8d4a60
Iyen Paul <pauliyen248@.com>
2026-07-31 02:09:09 +0100

    Generate artifact via AI - task: SQL

--- generated/airflow/fct_users_created_airflow_20260731.py
+++ generated/airflow/fct_users_created_airflow_20260731.py
@@ -1,13 +1,10 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.amazon.aws.operators.aws_sql import AWS_SQLOperator
-from airflow.providers.amazon.aws.helpers.sql import build_sql_query
-from airflow.utils.db import provide_session
-from airflow.hooks.base import BaseHook
-from airflow.exceptions import AirflowSkipException
-import logging
+from airflow.providers.amazon.aws.transfers.hive import HiveOperator
+from airflow.utils.dates import days_ago
 import json
+import logging
 
 default_args = {
     'owner': 'airflow',
@@ -20,84 +17,45 @@ default_args = {
 
 def create_fct_users_created(**kwargs):
     """
-    Creates the fct_users_created table.
+    Creates the fct_users_created table by querying the logging_events table.
+
+    :param kwargs:
+    :return:
     """
-    logging.info("Creating fct_users_created table")
-    query = """
+    logging.info('Creating fct_users_created table')
+    hive_query = """
         CREATE TABLE IF NOT EXISTS fct_users_created (
             user_id VARCHAR(100),
             user_name BOOLEAN
         );
+        
+        INSERT INTO fct_users_created
+        SELECT 
+            user_id,
+            user_name
+        FROM 
+            logging_events;
     """
-    AWS_SQLOperator(
+    HiveOperator(
         task_id='create_fct_users_created',
-        query=query,
-        aws_conn_id='aws_default',
-        database='hive',
-        params={}
-    ).execute(kwargs)
-
-def load_data(**kwargs):
-    """
-    Loads data from logging_events into fct_users_created.
-    """
-    logging.info("Loading data into fct_users_created table")
-    query = """
-        INSERT INTO fct_users_created (user_id, user_name)
-        SELECT user_id, user_name
-        FROM logging_events;
-    """
-    AWS_SQLOperator(
-        task_id='load_data',
-        query=query,
-        aws_conn_id='aws_default',
-        database='hive',
-        params={}
-    ).execute(kwargs)
-
-def check_data(**kwargs):
-    """
-    Checks if data was loaded successfully.
-    """
-    logging.info("Checking data in fct_users_created table")
-    query = """
-        SELECT COUNT(*) 
-        FROM fct_users_created;
-    """
-    result = AWS_SQLOperator(
-        task_id='check_data',
-        query=query,
-        aws_conn_id='aws_default',
-        database='hive',
-        params={}
-    ).execute(kwargs)
-    if result == 0:
-        raise AirflowSkipException("No data loaded")
+        hql=hive_query,
+        hive_cli_conn_id='hive_default',
+        dag=dag
+    )
 
 dag = DAG(
-    dag_id='fct_users_created',
+    'fct_users_created_dag',
     default_args=default_args,
-    description='A DAG to create and load the fct_users_created table',
+    description='A DAG to create the fct_users_created table',
     schedule_interval=timedelta(days=1),
-    start_date=datetime(2023, 1, 1),
+    start_date=days_ago(1),
+    tags=[],
     catchup=False,
 )
 
-create_table = PythonOperator(
-    task_id='create_table',
+create_fct_users_created_task = PythonOperator(
+    task_id='create_fct_users_created_task',
     python_callable=create_fct_users_created,
-    dag=dag
-)
-
-load_data_task = PythonOperator(
-    task_id='load_data',
-    python_callable=load_data,
-    dag=dag
-)
-
-check_data_task = PythonOperator(
-    task_id='check_data',
-    python_callable=check_data,
     dag=dag
 )
 
@@ -107,4 +65,4 @@ end_task = DummyOperator(
     dag=dag
 )
 
-create_table >> load_data_task >> check_data_task >> end_task
+create_fct_users_created_task >> end_task
--- generated/sql/fct_users_created_sql_20260731.sql
+++ generated/sql/fct_users_created_sql_20260731.sql
@@ -2,4 +2,4 @@ SELECT
     user_id,
     user_name
 FROM 
-    fct_users_created;
+    fct_users_created
