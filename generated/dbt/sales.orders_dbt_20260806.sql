7964f088600acb54b766b0e036586930663681bd
Paul Iyen <Pauliyen248@gmail.com>
2026-08-06 12:52:01 +0100

    Generate a dbt model for sales.orders

--- generated/dbt/sales.orders_dbt_20260806.sql
+++ generated/dbt/sales.orders_dbt_20260806.sql
@@ -1,7 +1,7 @@
 {{
   config(
     materialized = 'table',
-    alias = 'sales_orders'
+    alias = 'orders'
   )
 }}
 
@@ -10,17 +10,20 @@ WITH raw_orders AS (
     user_id,
     created_at,
     email
-  FROM {{ ref('raw_orders') }}
+  FROM 
+    {{ ref('raw_orders') }}
 ),
 raw_customers AS (
   SELECT 
     user_id
-  FROM {{ ref('raw_customers') }}
+  FROM 
+    {{ ref('raw_customers') }}
 )
-
 SELECT 
-  ro.user_id::BIGINT AS user_id,
-  ro.created_at::TIMESTAMP AS created_at,
-  ro.email::STRING AS email
-FROM raw_orders ro
-LEFT JOIN raw_customers rc ON ro.user_id = rc.user_id
+  raw_orders.user_id::BIGINT AS user_id,
+  raw_orders.created_at::TIMESTAMP AS created_at,
+  raw_orders.email::STRING AS email
+FROM 
+  raw_orders
+  LEFT JOIN raw_customers 
+  ON raw_orders.user_id = raw_customers.user_id
