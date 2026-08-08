9f9e06c7d897a5f08981fbf5f1c44a7d2a24a33b
Paul Iyen <Pauliyen248@gmail.com>
2026-08-08 17:58:23 +0100

    Generate Terraform infrastructure for the fct_users_created table and commit it to Git

--- generated/terraform/fct_users_created_terraform_20260808.tf
+++ generated/terraform/fct_users_created_terraform_20260808.tf
@@ -1,71 +1,110 @@
-# File: main.tf
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_rds_cluster" "fct_users_created" {
-  cluster_identifier = "fct-users-created-cluster"
-  database_name     = "fct_users_created_db"
-  master_username    = var.db_username
-  master_password    = var.db_password
-  engine             = "postgres"
-  engine_version     = "13.4"
-  port               = 5432
-}
-
-resource "aws_rds_cluster_instance" "fct_users_created" {
-  identifier         = "fct-users-created-instance"
-  cluster_identifier = aws_rds_cluster.fct_users_created.cluster_identifier
-  instance_class     = var.instance_class
-  engine             = aws_rds_cluster.fct_users_created.engine
-  engine_version     = aws_rds_cluster.fct_users_created.engine_version
-}
-
-resource "aws_s3_bucket" "fct_users_created" {
-  bucket = "fct-users-created-bucket"
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_object" "fct_users_created" {
-  bucket = aws_s3_bucket.fct_users_created.id
-  key    = "fct_users_created_schema.sql"
-  source = "${path.module}/fct_users_created_schema.sql"
-}
-
-resource "aws_glue_catalog_database" "fct_users_created" {
-  name = "fct_users_created_db"
-}
-
-resource "aws_glue_catalog_table" "fct_users_created" {
-  name          = "fct_users_created"
-  database_name = aws_glue_catalog_database.fct_users_created.name
-  table_type    = "EXTERNAL_TABLE"
-  owner         = "Paul"
-  storage_descriptor {
-    location = "s3://${aws_s3_bucket.fct_users_created.id}/fct_users_created/"
-    serdes {
-      name = "org.apache.hadoop.hive.serde2.OpenCSVSerde"
-    }
-    columns {
-      name = "id"
-      type = "int"
-    }
-  }
-}
-
-# File: variables.tf
-variable "aws_region" {
-  type = string
-}
-
+terraform
 variable "db_username" {
-  type = string
+  type        = string
+  sensitive   = true
 }
 
 variable "db_password" {
-  type = string
+  type        = string
+  sensitive   = true
 }
 
-variable "instance_class" {
-  type = string
+variable "db_host" {
+  type        = string
+}
+
+variable "db_port" {
+  type        = number
+}
+
+variable "db_name" {
+  type        = string
+}
+
+variable "environment" {
+  type        = string
+}
+
+variable "owners" {
+  type        = list(string)
+  default     = ["Paul"]
+}
+
+resource "aws_db_instance" "fct_users_created_db" {
+  identifier        = "fct-users-created-db"
+  instance_class    = "db.t2.micro"
+  engine             = "postgres"
+  username           = var.db_username
+  password           = var.db_password
+  port              = var.db_port
+  dbname             = var.db_name
+  vpc_security_group_ids = [aws_security_group.fct_users_created_db_sg.id]
+  publicly_accessible = false
+}
+
+resource "aws_db_subnet_group" "fct_users_created_db_subnet_group" {
+  name       = "fct-users-created-db-subnet-group"
+  subnet_ids = [aws_subnet.fct_users_created_db_subnet.id]
+}
+
+resource "aws_subnet" "fct_users_created_db_subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.fct_users_created_vpc.id
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_vpc" "fct_users_created_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_security_group" "fct_users_created_db_sg" {
+  name        = "fct-users-created-db-sg"
+  vpc_id      = aws_vpc.fct_users_created_vpc.id
+  ingress {
+    from_port   = var.db_port
+    to_port     = var.db_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "postgresql_database" "fct_users_created_db" {
+  name  = var.db_name
+  owner = "postgres"
+}
+
+resource "postgresql_table" "fct_users_created" {
+  database = postgresql_database.fct_users_created_db.name
+  name     = "fct_users_created"
+  owner    = "postgres"
+  table    = "fct_users_created"
+
+  column {
+    name = "id"
+    type = "integer"
+  }
+}
+
+output "db_instance_arn" {
+  value = aws_db_instance.fct_users_created_db.arn
+}
+
+output "db_instance_endpoint" {
+  value = aws_db_instance.fct_users_created_db.endpoint
+}
+
+output "db_instance_username" {
+  value = var.db_username
+  sensitive = true
+}
+
+output "db_instance_password" {
+  value = var.db_password
+  sensitive = true
 }
