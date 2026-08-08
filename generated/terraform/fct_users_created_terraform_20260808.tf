b3c2f73debad814d6775d0f1144649977d1d7934
Paul Iyen <Pauliyen248@gmail.com>
2026-08-08 17:57:31 +0100

    Generate Terraform infrastructure for the fct_users_created table and commit it to Git

--- generated/terraform/fct_users_created_terraform_20260808.tf
+++ generated/terraform/fct_users_created_terraform_20260808.tf
@@ -1,23 +1,71 @@
+# File: main.tf
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
 
-resource "aws_dynamodb_table" "fct_users_created" {
-  name           = "fct_users_created"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+resource "aws_rds_cluster" "fct_users_created" {
+  cluster_identifier = "fct-users-created-cluster"
+  database_name     = "fct_users_created_db"
+  master_username    = var.db_username
+  master_password    = var.db_password
+  engine             = "postgres"
+  engine_version     = "13.4"
+  port               = 5432
+}
 
-  attribute {
-    name = "id"
-    type = "N"
-  }
+resource "aws_rds_cluster_instance" "fct_users_created" {
+  identifier         = "fct-users-created-instance"
+  cluster_identifier = aws_rds_cluster.fct_users_created.cluster_identifier
+  instance_class     = var.instance_class
+  engine             = aws_rds_cluster.fct_users_created.engine
+  engine_version     = aws_rds_cluster.fct_users_created.engine_version
+}
 
-  tags = {
-    Owners = "Paul"
+resource "aws_s3_bucket" "fct_users_created" {
+  bucket = "fct-users-created-bucket"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "fct_users_created" {
+  bucket = aws_s3_bucket.fct_users_created.id
+  key    = "fct_users_created_schema.sql"
+  source = "${path.module}/fct_users_created_schema.sql"
+}
+
+resource "aws_glue_catalog_database" "fct_users_created" {
+  name = "fct_users_created_db"
+}
+
+resource "aws_glue_catalog_table" "fct_users_created" {
+  name          = "fct_users_created"
+  database_name = aws_glue_catalog_database.fct_users_created.name
+  table_type    = "EXTERNAL_TABLE"
+  owner         = "Paul"
+  storage_descriptor {
+    location = "s3://${aws_s3_bucket.fct_users_created.id}/fct_users_created/"
+    serdes {
+      name = "org.apache.hadoop.hive.serde2.OpenCSVSerde"
+    }
+    columns {
+      name = "id"
+      type = "int"
+    }
   }
 }
 
-variable "region" {
-  type        = string
-  description = "AWS region"
+# File: variables.tf
+variable "aws_region" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "instance_class" {
+  type = string
 }
